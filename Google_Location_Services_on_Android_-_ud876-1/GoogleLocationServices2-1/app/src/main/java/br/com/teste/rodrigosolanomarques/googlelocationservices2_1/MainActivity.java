c695ff09da9e81c90ad5f82d80eebc53fc726262
Rodrigo Solano Marques <rodrigosolanomarques@gmail.com>
2017-07-07 10:35:56 -0300

    Alteração - Lição 3 - Transformar um aplicativo que recebe apenas uma atualização de localização para receber a localização a cada 1 segundo.

--- Google_Location_Services_on_Android_-_ud876-1/GoogleLocationServices2-1/app/src/main/java/br/com/teste/rodrigosolanomarques/googlelocationservices2_1/MainActivity.java
+++ Google_Location_Services_on_Android_-_ud876-1/GoogleLocationServices2-1/app/src/main/java/br/com/teste/rodrigosolanomarques/googlelocationservices2_1/MainActivity.java
@@ -1,13 +1,96 @@
 package br.com.teste.rodrigosolanomarques.googlelocationservices2_1;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private TextView txtLatitude;
+    private TextView txtLongitude;
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+
+    public final String TAG_LOG = "GoogleLocationServices";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtLatitude = (TextView) findViewById(R.id.txtLatitude);
+        txtLongitude = (TextView) findViewById(R.id.txtLongitude);
+
+        buildGoogleApiClient();
+
+
+    }
+
+    private void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mGoogleApiClient.isConnected())
+            mGoogleApiClient.disconnect();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        mLocationRequest = new LocationRequest().setInterval(1000).setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG_LOG, "Dispositivo sem permissão");
+            return;
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG_LOG, "Conexão suspensa com a Google API");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG_LOG, "Conexão falhou com a Google API");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        txtLatitude.setText(Double.toString(location.getLatitude()));
+        txtLongitude.setText(Double.toString(location.getLongitude()));
     }
 }
